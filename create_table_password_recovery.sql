9378e290f6cf46feae4297c96723db91df94b4ae
J.B.T. Mensink <julianmensink25@gmail.com>
2025-04-07 13:52:54 +0200

    Remove abces password_recovery

--- create_table_password_recovery.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE password_recovery (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
-    token VARCHAR(255) NOT NULL,
-    expires DATETIME NOT NULL,
-    FOREIGN KEY (email) REFERENCES user_info(email)
-);
