344d7efa635db06b024a5631617924659bdff425
Humper04 <julianmensink25@gmail.com>
2025-05-19 13:32:56 +0200

    Slight modifications
    For style: set color to #999 and removed text decoration for a
    For profile: everything till and including head above the php block, so the base info is always included
    For index: removed the part that redircts to login when not logged in, also made the top about the same as "User Profile" (except the name) and made both pages look better

--- index.php
+++ index.php
@@ -1,19 +1,18 @@
-<?php
-session_start();
-if (!isset($_SESSION['user_logged_in']) || $_SESSION['user_logged_in'] !== true) {
-    header('Location: login.php'); // Redirect to login page if not logged in
-    exit();
-}
-?>
-
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="./css/style.css">
     <title>Index</title>
 </head>
-
 <?php
-echo "Welcome to the Home Page! <br>";
-echo "Go to your <a href='profile.php'>Profile</a><br>";
-echo "Go to <a href='leaderboard.php'>Leaderboard</a>";
+session_start();
+if (!isset($_SESSION['user_logged_in']) || $_SESSION['user_logged_in'] !== true) {
+    echo "You are not logged in. <br>";
+    echo "You can <a href='login.php'>login</a> here.";
+} else {
+    echo "Welcome to the Home Page! <br>";
+    echo "You can view your <a href='profile.php'>profile</a> Here";
+}
+echo "<br>You can view the <a href='leaderboard.php'>leaderboard</a> here.";
 ?>
